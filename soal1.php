a9e5304c681b4009d4f8dc36acdb37b842fc0072
abiru sabil <m.abirusabil1@gmail.com>
2025-09-10 19:11:45 +0700

    update soal1

--- soal1.php
+++ soal1.php
@@ -2,16 +2,24 @@
 
 $jml = $_GET['jml'];
 echo "<table border=1>\n";
-for ($a = $jml; $a > 0; $a--)
-{
-  echo "<tr>\n";
-  for ($b = $a; $b > 0; $b--)
-  {
-    echo "<td>$b</td>";
-  }
-  
-  echo "</tr>\n";
-}
-echo "</table>";
 
+for ($a = $jml; $a > 0; $a--) {
+    // Hitung total untuk baris ini
+    $total = 0;
+    for ($b = $a; $b > 0; $b--) {
+        $total += $b;
+    }
+
+    // Cetak baris total
+    echo "<tr><td colspan='$jml'>TOTAL: $total</td></tr>\n";
+
+    // Cetak baris angka
+    echo "<tr>\n";
+    for ($b = $a; $b > 0; $b--) {
+        echo "<td>$b</td>";
+    }
+    echo "</tr>\n";
+}
+
+echo "</table>";
 ?>
